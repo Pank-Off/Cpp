0a5220dca46adbb172b37f4333a892b1d5a4cc62
Pank-Off <59021891+Pank-Off@users.noreply.github.com>
2020-02-06 18:49:35 +0300

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,20 @@
+#ifndef Timer_H
+#define Timer_H
+#include <chrono>
+
+class Timer {
+private:
+	using clock_t=std::chrono::high_resolution_clock;
+	using second_t=std::chrono::duration<double, std::ratio<1>>;
+
+	std::chrono::time_point<clock_t> m_beg;
+public:
+	Timer() : m_beg(clock_t::now()) {}
+
+	void reset();
+
+	double elapsed() const;
+};
+
+#endif // !Timer_H
+
